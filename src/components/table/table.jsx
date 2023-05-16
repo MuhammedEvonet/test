d8ddde66c779ee74e45cee979b149389aff33af6
MuhammedTahaCer <cermuhammed720@gmail.com>
2023-05-16 16:08:13 +0300

    .

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -7,7 +7,6 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
 import { v4 as uuid } from 'uuid';
-import React, { useEffect, useState } from 'react';
 
 
 function RndKey() {
